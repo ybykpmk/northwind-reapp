63cca81f220165b35ed13fe255060b3eb3596aec
Test Kullanicisi <test@gmail.com>
2023-05-31 14:46:15 +0300

    third commit by master

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,12 +3,14 @@ import changeCategoryReducer from "./changeCategoryReducer";
 import categoryListReducer from "./categoryListReducer";
 import productListReducer from "./productListReducer";
 import cartReducer from "./cartReducer";
+import saveProductReducer from "./saveProductReducer";
 
 const rootReducer=combineReducers({
     changeCategoryReducer:changeCategoryReducer,
     categoryListReducer:categoryListReducer,
     productListReducer:productListReducer,
-    cartReducer:cartReducer
+    cartReducer:cartReducer,
+    saveProductReducer:saveProductReducer
 })
 
 export default rootReducer;
